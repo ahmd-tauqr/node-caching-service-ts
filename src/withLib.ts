4bdaca9eee44e4c3bd081f63cd3315dcfc6f4238
ahmd-tauqr <ahmd.tauqr@gmail.com>
2024-04-17 17:57:51 +0530

    added option with node-cache and without external library

---
+++ src/withLib.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+import axios from 'axios';
+import NodeCache from 'node-cache';
+
+// Define the interface for cache entries
+interface CacheEntry {
+  data: Buffer;
+  cachedAt: Date;
+  expiresAt: Date;
+}
+
+const app = express();
+const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
+
+app.get('/cache/:encodedUrl', async (req, res) => {
+  const { encodedUrl } = req.params;
+  const url = Buffer.from(encodedUrl, 'base64').toString('ascii');
+
+  try {
+    let cacheEntry = cache.get<CacheEntry>(url);
+    let cacheHit = true;
+
+    if (!cacheEntry) {
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+      const cachedAt = new Date();
+      let expiresAt = new Date(cachedAt.getTime() + 533280 * 1000); // Calculate the expires time
+
+      // Create new cache entry
+      cacheEntry = {
+        data: response.data,
+        cachedAt: cachedAt,
+        expiresAt: expiresAt,
+      };
+
+      // Set the cache with the new entry
+      cache.set(url, cacheEntry);
+      cacheHit = false;
+    }
+
+    // Set headers based on cache metadata
+    res.setHeader('Content-Type', 'image/jpeg'); // Set this dynamically based on the actual content type
+    res.setHeader('Last-Modified', cacheEntry.cachedAt.toUTCString());
+    res.setHeader('Cache-Control', 'public, max-age=533280');
+    res.setHeader('Expires', cacheEntry.expiresAt.toUTCString());
+    res.setHeader('X-Cache', cacheHit ? 'HIT' : 'MISS');
+
+    res.send(cacheEntry.data);
+  } catch (error) {
+    console.error('Failed to retrieve image:', error);
+    res.status(500).send('Error retrieving the image.');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
